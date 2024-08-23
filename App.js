a42f352e641f89097f5692d09e590a4d3d3f5a23
jmdiaz23 <jorgemariodiaz7@gmail.com>
2024-08-23 16:58:23 -0500

    agregando area segura

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import Constants from "expo-constants";
-import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
-import { getLatestGames } from "./lib/metacritic";
+
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Main } from "./components/Main";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => {
-      setGames(games);
-    });
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="ligth" />
-
-      <ScrollView>
-        {games.map((game) => (
-          <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.score}>{game.score}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="ligth" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -37,30 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
-    padding: 12,
-  },
-  card: {
-    marginBottom: 42,
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#fff",
-    marginTop: 10,
-  },
-  description: {
-    fontSize: 16,
-    color: "#eee",
-  },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 10,
+    paddingHorizontal: 12,
   },
 });
