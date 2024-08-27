cffaa6b9c82fd2898fb558d1ace1beba54490223
jmdiaz23 <jorgemariodiaz7@gmail.com>
2024-08-27 08:26:36 -0500

    agregando el about

---
+++ app/about.js
@@ -0,0 +1,9 @@
+import { ScrollView, Text } from "react-native";
+
+export default function About() {
+  return (
+    <ScrollView>
+      <Text className="text-white font-bold mb-8 text-2xl">About</Text>
+    </ScrollView>
+  );
+}
